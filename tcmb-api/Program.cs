3db2fefdbde8b191de99430da17c0313d711901c
murat <muratsaygili1@gmail.com>
2024-04-20 16:45:27 +0300

    key parameter removed from url and added to request header for exchange-rate endpoint

--- tcmb-api/Program.cs
+++ tcmb-api/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.ServiceModel;
 using System.Text.Json;
 using System.Web;
@@ -12,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TCMB Api", Version = "v1", Contact = new OpenApiContact() { Email = "muratsaygili1@gmail.com", Name = "Murat Saygýlý" } });
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "TCMB Api", Version = "v1", Contact = new OpenApiContact() { Email = "muratsaygili1@gmail.com", Name = "Murat Saygýlý" } });
 });
 
 var app = builder.Build();
@@ -20,126 +21,125 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+	app.UseDeveloperExceptionPage();
 
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1");
-        c.RoutePrefix = string.Empty;
-    });
+	app.UseSwagger();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1");
+		c.RoutePrefix = string.Empty;
+	});
 }
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("exchange-rate", (string currencyCodes) =>
 {
-    currencyCodes = string.IsNullOrEmpty(currencyCodes) ? "USD,EUR,GBP" : currencyCodes.ToUpper();
-    /*
+	currencyCodes = string.IsNullOrEmpty(currencyCodes) ? "USD,EUR,GBP" : currencyCodes.ToUpper();
+	/*
      * api key almak için https://evds2.tcmb.gov.tr/index.php?/evds/editProfile
      * adresine giriþ yapýlýr.
      */
-    //get evds api key from appsettings.json
-    var key = builder.Configuration["Evds2ApiKey"];
+	//get evds api key from appsettings.json
+	var key = builder.Configuration["Evds2ApiKey"];
 
-    var currencies = currencyCodes.Split(',').Select(currencyCode => new Currency() { CurrencyCode = currencyCode }).ToList();
+	var currencies = currencyCodes.Split(',').Select(currencyCode => new Currency() { CurrencyCode = currencyCode }).ToList();
 
-    var seriesList = new List<string>();
+	var seriesList = new List<string>();
 
-    currencies.ForEach(currency =>
-    {
-        seriesList.Add($"TP.DK.{currency.CurrencyCode}.A");
-        seriesList.Add($"TP.DK.{currency.CurrencyCode}.S");
-        seriesList.Add($"TP.DK.{currency.CurrencyCode}.A.EF");
-        seriesList.Add($"TP.DK.{currency.CurrencyCode}.S.EF");
-    });
+	currencies.ForEach(currency =>
+	{
+		seriesList.Add($"TP.DK.{currency.CurrencyCode}.A");
+		seriesList.Add($"TP.DK.{currency.CurrencyCode}.S");
+		seriesList.Add($"TP.DK.{currency.CurrencyCode}.A.EF");
+		seriesList.Add($"TP.DK.{currency.CurrencyCode}.S.EF");
+	});
 
+	var uriBuilder = new UriBuilder("https://evds2.tcmb.gov.tr/");
+	var queryString = HttpUtility.ParseQueryString(uriBuilder.Query);
+	queryString["series"] = string.Join("-", seriesList);
+	queryString["startDate"] = DateTime.Now.AddDays(-10).ToString("dd-MM-yyyy");
+	queryString["endDate"] = DateTime.Now.ToString("dd-MM-yyyy");
+	queryString["type"] = "xml";
+	uriBuilder.Path = string.Concat("service/evds/", queryString);
 
+	var request = WebRequest.Create(uriBuilder.ToString());
+	request.Headers.Add("key", key);
+	using var response = request.GetResponse();
+	using var stream = response.GetResponseStream();
+	var xml = new XmlDocument();
+	xml.Load(stream);
 
-    var uriBuilder = new UriBuilder("https://evds2.tcmb.gov.tr/");
-    var queryString = HttpUtility.ParseQueryString(uriBuilder.Query);
-    queryString["series"] = string.Join("-", seriesList);
-    queryString["startDate"] = DateTime.Now.AddDays(-10).ToString("dd-MM-yyyy");
-    queryString["endDate"] = DateTime.Now.ToString("dd-MM-yyyy");
-    queryString["type"] = "xml";
-    queryString["key"] = key;
-    uriBuilder.Path = string.Concat("service/evds/", queryString);
+	foreach (var currency in currencies)
+	{
+		currency.ForexBuying = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_A");
+		currency.ForexSelling = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_S");
+		currency.BanknoteBuying = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_A_EF");
+		currency.BanknoteSelling = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_S_EF");
+	}
 
-    var xml = new XmlDocument();
-    xml.Load(new XmlTextReader(uriBuilder.ToString()));
-
-    foreach (var currency in currencies)
-    {
-        currency.ForexBuying = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_A");
-        currency.ForexSelling = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_S");
-        currency.BanknoteBuying = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_A_EF");
-        currency.BanknoteSelling = xml.GetCurrencyValueFromXml($"TP_DK_{currency.CurrencyCode}_S_EF");
-
-
-    }
-
-    return currencies;
+	return currencies;
 })
-    .WithDescription("get exchange rates for Turkish Lira")
-    .WithOpenApi();
+	.WithDescription("get exchange rates for Turkish Lira")
+	.WithOpenApi();
 
 
 
 app.MapGet("/bank-branch", async (httpContext) =>
 {
-    string json;
-    TUMCvp? tumCevap;
-    //check if file exists and created today
-    if (File.Exists("tumCevap.json") && File.GetCreationTime("tumCevap.json").Date == DateTime.Now.Date)
-    {
-        json = await File.ReadAllTextAsync("tumCevap.json");
-        tumCevap = JsonSerializer.Deserialize<TUMCvp>(json);
-        await httpContext.Response.WriteAsJsonAsync(tumCevap.bankaSubeleri.Select(x => new
-        {
-            BankCode = x.bKd,
-            BankName = x.banka.bAd,
-            Branchs = x.sube.Select(x => new
-            {
-                BranchCode = x.sKd,
-                BranchName = x.sAd
-            })
-        }).ToList());
-        return;
-    }
+	string json;
+	TUMCvp? tumCevap;
+	//check if file exists and created today
+	if (File.Exists("tumCevap.json") && File.GetCreationTime("tumCevap.json").Date == DateTime.Now.Date)
+	{
+		json = await File.ReadAllTextAsync("tumCevap.json");
+		tumCevap = JsonSerializer.Deserialize<TUMCvp>(json);
+		await httpContext.Response.WriteAsJsonAsync(tumCevap.bankaSubeleri.Select(x => new
+		{
+			BankCode = x.bKd,
+			BankName = x.banka.bAd,
+			Branchs = x.sube.Select(x => new
+			{
+				BranchCode = x.sKd,
+				BranchName = x.sAd
+			})
+		}).ToList());
+		return;
+	}
 
 
-    var service = new bankaSubeOkuClient()
-    {
-        Endpoint =
-        {
-            Address = new EndpointAddress("https://appg.tcmb.gov.tr/mbnbasuse/services/bankaSubeOku"),
-            Binding = new BasicHttpsBinding()
-            {
-                MaxReceivedMessageSize = 10000000
-            }
-        }
-    };
+	var service = new bankaSubeOkuClient()
+	{
+		Endpoint =
+		{
+			Address = new EndpointAddress("https://appg.tcmb.gov.tr/mbnbasuse/services/bankaSubeOku"),
+			Binding = new BasicHttpsBinding()
+			{
+				MaxReceivedMessageSize = 10000000
+			}
+		}
+	};
 
-    var response = await service.bankaSubeOkuAsync(new bankaSubeOkuGirdi("TUM", null, null));
+	var response = await service.bankaSubeOkuAsync(new bankaSubeOkuGirdi("TUM", null, null));
 
-    tumCevap = (TUMCvp)response.Item;
+	tumCevap = (TUMCvp)response.Item;
 
-    //write tumCevap to json file
-    json = JsonSerializer.Serialize(tumCevap);
-    await File.WriteAllTextAsync("tumCevap.json", json);
+	//write tumCevap to json file
+	json = JsonSerializer.Serialize(tumCevap);
+	await File.WriteAllTextAsync("tumCevap.json", json);
 
-    await httpContext.Response.WriteAsJsonAsync(tumCevap.bankaSubeleri.Select(x => new
-    {
-        BankCode = x.bKd,
-        BankName = x.banka.bAd,
-        Branchs = x.sube.Select(x => new
-        {
-            BranchCode = x.sKd,
-            BranchName = x.sAd
-        })
-    }).ToList());
+	await httpContext.Response.WriteAsJsonAsync(tumCevap.bankaSubeleri.Select(x => new
+	{
+		BankCode = x.bKd,
+		BankName = x.banka.bAd,
+		Branchs = x.sube.Select(x => new
+		{
+			BranchCode = x.sKd,
+			BranchName = x.sAd
+		})
+	}).ToList());
 })
-    .WithDescription("get bank and their branches of Turkey")
-    .WithOpenApi(); ;
+	.WithDescription("get bank and their branches of Turkey")
+	.WithOpenApi(); ;
 
 app.Run();
